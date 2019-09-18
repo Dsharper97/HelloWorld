dbe05dfea45f60b7e3100b5d85180542fe5b4223
Derrick Sharper <pfldsharper@gmail.com>
2019-09-18 11:13:25 -0400

    Retrieved button and text object from the activity main by id.
    Changed the text of the button from "button" to "Change Text"
    
    Imported Android.view.View, Android.widget.Button and Android.widget.TextView in order to use methods associated
    with the view, textview and button.
    
    Set an On click listener on button to change TextViewt text(txt) txt to "You have clicked he button" when clicked.

--- app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -2,12 +2,30 @@ package edu.temple.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Add button to change text
+        //get the button and text view
+        txt = findViewById(R.id.textView);
+        Button btn = findViewById(R.id.btnChangeTxt);
+
+        //set what the button does.
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txt.setText("You have clicked the button");
+            }
+        });
+
     }
 }
